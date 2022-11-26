51f58ecb13a2b14db441e2411d623e913c211e88
Tareq35 <a.t.tariqulislam@gmail.com>
2022-11-26 23:42:29 +0600

    heading added

---
+++ src/Pages/Home/ProductCategories/ProductCategories.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ProductCategories = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default ProductCategories;
--- src/Pages/Home/Slider/Slider.js
+++ src/Pages/Home/Slider/Slider.js
@@ -8,13 +8,61 @@ const Slider = () => {
         <div>
             <div className="carousel w-full">
                 <div id="slide1" className="carousel-item relative w-full h-1/2">
-                    <img src={slide1} className="w-full h-[700px]" alt='' />
+
+                    <img src={slide1} className="w-full h-[800px]" alt='' />
+                    <div className='absolute flex h-full w-full items-center justify-center'>
+                        <div className='text-center'>
+
+                            <h1 className='text-5xl font-bold text-white text-center'>Welcome to LapSell Corner</h1>
+
+                            <p className='w-1/2 text-xl font-medium text-white mx-auto mt-5'>Buy second-hand, Laptops on LapSell Corner, the world's best online marketplace. top brands, low prices & free shipping on many items.</p>
+                        </div>
+                        
+                    </div>
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+
                         <a href="#slide3" className="btn btn-circle">❮</a>
                         <a href="#slide2" className="btn btn-circle">❯</a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full h-1/2">
+
+                    <img src={slide2} className="w-full h-[800px]" alt='' />
+                    <div className='absolute flex h-full w-full items-center justify-center'>
+                        <div className='text-center'>
+
+                            <h1 className='text-5xl font-bold text-white text-center'>Welcome to LapSell Corner</h1>
+
+                            <p className='w-1/2 text-xl font-medium text-white mx-auto mt-5'>Buy second-hand, Laptops on LapSell Corner, the world's best online marketplace. top brands, low prices & free shipping on many items.</p>
+                        </div>
+                        
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide1" className="btn btn-circle">❮</a>
+                        <a href="#slide3" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+                <div id="slide3" className="carousel-item relative w-full h-1/2">
+
+                    <img src={slide3} className="w-full h-[800px]" alt='' />
+                    <div className='absolute flex h-full w-full items-center justify-center'>
+                        <div className='text-center'>
+
+                            <h1 className='text-5xl font-bold text-white text-center'>Welcome to LapSell Corner</h1>
+
+                            <p className='w-1/2 text-xl font-medium text-white mx-auto mt-5'>Buy second-hand, Laptops on LapSell Corner, the world's best online marketplace. top brands, low prices & free shipping on many items.</p>
+                        </div>
+                        
+                    </div>
+                    <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+                        <a href="#slide2" className="btn btn-circle">❮</a>
+                        <a href="#slide1" className="btn btn-circle">❯</a>
+                    </div>
+                </div>
+
+
+
+                {/* <div id="slide2" className="carousel-item relative w-full h-1/2">
                     <img src={slide2} className="w-full  h-[700px]" alt='' />
                     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
                         <a href="#slide1" className="btn btn-circle">❮</a>
@@ -28,7 +76,7 @@ const Slider = () => {
                         <a href="#slide2" className="btn btn-circle">❮</a>
                         <a href="#slide1" className="btn btn-circle">❯</a>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     );
