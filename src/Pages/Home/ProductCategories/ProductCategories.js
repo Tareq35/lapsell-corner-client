84cae3f3567bba567477ae578a4a2d42232a55ee
Tareq35 <a.t.tariqulislam@gmail.com>
2022-11-27 13:30:04 +0600

    dynamically product category added

--- src/Pages/Home/ProductCategories/ProductCategories.js
+++ src/Pages/Home/ProductCategories/ProductCategories.js
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ProductCategoryItem from './ProductCategoryItem';
 
 const ProductCategories = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        fetch('categories.json')
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <section className='my-20 w-9/12 mx-auto'>
             <h1 className='text-center text-4xl mb-10 font-bold'>Product Categories</h1>
-
-            <ProductCategoryItem />
-
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
+                {
+                    categories.map(category => <ProductCategoryItem
+                        key={category._id}
+                        productCategory={category}
+                    ></ProductCategoryItem>)
+                }
+            </div>
         </section>
     );
 };
--- src/Pages/Home/ProductCategories/ProductCategoryItem.js
+++ src/Pages/Home/ProductCategories/ProductCategoryItem.js
@@ -1,119 +1,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const ProductCategoryItem = () => {
+const ProductCategoryItem = ({ productCategory }) => {
+    const { product_category, brandImg } = productCategory;
     return (
-        <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-6'>
-            <div className="max-w-sm mx-auto rounded-lg border">
-                <Link to="" className="" rel="">
-                    <div className="rounded-lg shadow-lg bg-white">
-                        <div className="overflow-hidden">
-                            <img
-                                className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
-                                src="https://i.ibb.co/rxqQZJq/79183-1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <div className="p-5 bg-blue-200 rounded-b-lg">
-                            <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
-                                HP
-                            </h5>
-                        </div>
+
+        <div className="max-w-sm mx-auto rounded-lg border">
+            <Link to="" className="" rel="">
+                <div className="rounded-lg shadow-lg bg-white">
+                    <div className="overflow-hidden">
+                        <img
+                            className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
+                            src={brandImg}
+                            alt=""
+                        />
                     </div>
-                </Link>
-            </div>
-            <div className="max-w-sm mx-auto rounded-lg border">
-                <Link to="" className="" rel="">
-                    <div className="rounded-lg shadow-lg bg-white">
-                        <div className="overflow-hidden">
-                            <img
-                                className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
-                                src="https://i.ibb.co/rxqQZJq/79183-1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <div className="p-5 bg-blue-200 rounded-b-lg">
-                            <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
-                                HP
-                            </h5>
-                        </div>
+                    <div className="p-5 bg-blue-200 rounded-b-lg">
+                        <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
+                            {product_category}
+                        </h5>
                     </div>
-                </Link>
-            </div>
-            <div className="max-w-sm mx-auto rounded-lg border">
-                <Link to="" className="" rel="">
-                    <div className="rounded-lg shadow-lg bg-white">
-                        <div className="overflow-hidden">
-                            <img
-                                className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
-                                src="https://i.ibb.co/rxqQZJq/79183-1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <div className="p-5 bg-blue-200 rounded-b-lg">
-                            <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
-                                HP
-                            </h5>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="max-w-sm mx-auto rounded-lg border">
-                <Link to="" className="" rel="">
-                    <div className="rounded-lg shadow-lg bg-white">
-                        <div className="overflow-hidden">
-                            <img
-                                className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
-                                src="https://i.ibb.co/rxqQZJq/79183-1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <div className="p-5 bg-blue-200 rounded-b-lg">
-                            <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
-                                HP
-                            </h5>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="max-w-sm mx-auto rounded-lg border">
-                <Link to="" className="" rel="">
-                    <div className="rounded-lg shadow-lg bg-white">
-                        <div className="overflow-hidden">
-                            <img
-                                className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
-                                src="https://i.ibb.co/rxqQZJq/79183-1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <div className="p-5 bg-blue-200 rounded-b-lg">
-                            <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
-                                HP
-                            </h5>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            <div className="max-w-sm mx-auto rounded-lg border">
-                <Link to="" className="" rel="">
-                    <div className="rounded-lg shadow-lg bg-white">
-                        <div className="overflow-hidden">
-                            <img
-                                className="rounded-t-lg transition duration-300 h-[286.5px] transform hover:scale-110"
-                                src="https://i.ibb.co/rxqQZJq/79183-1.jpg"
-                                alt=""
-                            />
-                        </div>
-                        <div className="p-5 bg-blue-200 rounded-b-lg">
-                            <h5 className="text-gray-900 text-xl font-medium mb-2 line-clamp-2">
-                                HP
-                            </h5>
-                        </div>
-                    </div>
-                </Link>
-            </div>
-            
+                </div>
+            </Link>
         </div>
+
+
+
     );
 };
 
